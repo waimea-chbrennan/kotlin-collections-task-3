33ffc807b14ec21121bdbf95c6aa9457c55cc1b1
c h brennan <chbrennan@waimea.school.nz>
2025-03-04 15:06:05 +1300

    feat: add error checking

--- src/Main.kt
+++ src/Main.kt
@@ -1,3 +1,5 @@
+import kotlin.math.max
+
 /**
  * Kotlin Collections Task 3 - Monkey Errors
  *
@@ -94,7 +96,128 @@ fun main() {
 }
 
 
+fun setupCages(): MutableList<String> {
+    val cageList = mutableListOf<String>()
+    for (i in 1..NUMCAGES) cageList.add(EMPTY)
+    return cageList
+}
 
+
+/**
+ * Returns the number of monkeys found in the given cage list
+ */
+fun monkeyCount(cageList: List<String>): Int {
+    return cageList.filter { cage -> cage != EMPTY }.size
+}
+
+
+/**
+ * Returns the number of cages that are empty in the given cage list
+ */
+fun emptyCount(cageList: List<String>): Int {
+    return cageList.filter { cage -> cage == EMPTY }.size
+}
+
+
+/**
+ * Show all cages from the given list, formatted as a horizontal table:
+ *
+ * +--------+--------+--------+--------+----
+ * | Cage 1 | Cage 2 | Cage 3 | Cage 4 | Etc.
+ * +--------+--------+--------+--------+----
+ * | Kevin  | ---    | Samson | Pam    | Etc.
+ * +--------+--------+--------+--------+----
+ *
+ * Tip: the String.padEnd(N) function will help you here
+ */
+fun showMonkeyCages(cageList: List<String>) {
+    var banner = "+"
+    var namesAndPipes = "|"
+    var cagesAndPipes = "|"
+
+    val longestEntry = max(cageList.maxBy{it.length}.length, "Cage $NUMCAGES".length)
+
+    cageList.forEachIndexed { index, cageOccupant ->
+        //Prepare cage num | Cage 1 | Cage 2| ...
+        val cageDescriptor = "Cage ${index+1}"
+        cagesAndPipes += " ${cageDescriptor.padEnd(longestEntry, ' ')} |"
+
+        //Prepare banner +--------+--------+
+        banner += "-".repeat(longestEntry + 2)
+        banner += "+"
+
+        //Prepare names | monkey | monkey | monkey |
+        namesAndPipes += " ${cageOccupant.padEnd(longestEntry, ' ')} |"
+    }
+
+    println(banner)
+    println(cagesAndPipes)
+    println(banner)
+    println(namesAndPipes)
+    println(banner)
+}
+
+
+
+
+
+
+
+
+/**
+ * Put a given monkey into the specified cage number (1...MAX)
+ */
+fun placeMonkeyInCage(cageList: MutableList<String>, cageNum: Int, name: String) {
+    println("+++ Putting $name into cage $cageNum")
+    if(checkCageError(cageNum,"PLACING")) return
+    if(name.isBlank()) {
+        println("ERROR PLACING MONKEY: Name cannot be blank")
+        return
+    }
+
+    cageList.removeAt(cageNum-1)
+    cageList.add(cageNum-1,name)
+}
+
+
+
+/**
+ * Make a given cage empty (if a monkey was in it, it's gone now!)
+ */
+fun clearCage(cageList: MutableList<String>, cageNum: Int) {
+    println("--- Clearing cage $cageNum")
+    if(checkCageError(cageNum,"CLEARING")) return
+
+    cageList.removeAt(cageNum-1)
+    cageList.add(cageNum-1,EMPTY)
+}
+
+
+
+/**
+ * Swap the contents of two given cages.
+ *
+ * If one was full and the other empty, then the monkey just swaps
+ * into the empty cage.
+ */
+
+fun swapCages(cageList: MutableList<String>, cageNum1: Int, cageNum2: Int) {
+    println("<-> Swapping cages $cageNum1 and $cageNum2")
+
+    if(checkCageError(cageNum1,"SWAPPING")) return
+    if(checkCageError(cageNum2,"SWAPPING")) return
+
+
+    cageList[cageNum1-1] = cageList[cageNum2-1].also { cageList[cageNum2-1] = cageList[cageNum1-1] }
+}
+
+fun checkCageError(cageNum: Int, errorVerb: String): Boolean {
+    if(cageNum !in 1..NUMCAGES) {
+        println("ERROR $errorVerb CAGE: Cage $cageNum is out of bounds")
+        return true
+    }
+    return false
+}
 
 
 
